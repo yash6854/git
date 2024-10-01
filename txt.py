cc5f03f4a5eabfb459da903d7bcccff0096eb2ad
Yash Vijay Maral <maral.yash.2k@gmail.com>
2024-10-01 16:28:50 +0530

    removed spaces

--- txt.py
+++ txt.py
@@ -3,16 +3,13 @@ class Person:
         self.name = name
         self.college = college
 
-        def show(self):
-            print("This is Parent Class")
-
 class Degree(Person):
     def __init__(self,name,college,id):
         super().__init__(name,college)
         self.id = id
-
         def show1(self):
             print("This is a child Class")
+
 class toppers(Person):
     def __init__(self,name,college,rank):
         super().__init__(name,college)
