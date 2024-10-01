048137570dfc8a25a7e4570bcdd1f4915e88f8f5
Yash Vijay Maral <maral.yash.2k@gmail.com>
2024-10-01 11:28:37 +0530

    Inital commit

---
+++ test1.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self,name,college):
+        self.name = name
+        self.college = college
+
+    def show1(self):
+        print("THis is the parent class")
+
+    
